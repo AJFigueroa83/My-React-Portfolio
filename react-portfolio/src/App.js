c710cd1f970ee3d1f3cd661ee368c86a080e4bb5
Alfredo Figueroa <alfredojfigueroa@gmail.com>
2022-07-20 18:42:11 -0400

    created initial folder structure and started adding some code to the components.

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import 'cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css';
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-portfolio/src/components/AboutMe.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function AboutMe() {
+
+    return (
+        <div class="hero-body" id="heroBody">
+            <div class="container has-text-centered" id="title">
+                <img class="has-ratio" width='256' height='256' src="./assets/images/IMG_3905 2.jpg" alt="profile picture" id="profilePic"></img>  
+                <p class="title" id="name">
+                    <p>Hello! I have been in the hospitality industry for over 20 years. In today's everchanging world, a new path has emerged for me. I am now embarkign on a career change into the world of web development. Curretnly I am a student in the Georgia Tech Coding bootcamp and I am looking forward to this exciting career.</p>
+                    <p>I am a Full-stack web developer leveraging a hospitality management background to build a more intuitive user experience on the web. Recently earned a certificate in full stack development from Georgia Tech Coding Bootcamp, with newly developed skills in JavaScript, CSS, React.js, Node.js and responsive web design. With each project, the aim is to become better at developing more skills. Have worked on a team of three to develop a search/locator app that helps locals and travelers find destination breweries. Very excited to leverage my skills as part of a fast-paced, quality-driven team to build better experiences on the web.
+                    </p>
+                    <p>I look forward to taking my skills and put them into practice at an fun, exciting and energetic company where I can collaborate with others and continue learning new skills in this industry. Let's code!!</p>
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default AboutMe;
