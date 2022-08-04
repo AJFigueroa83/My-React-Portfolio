6f8760405a0806252ff20482863d5a0d74dc6945
Alfredo Figueroa <alfredojfigueroa@gmail.com>
2022-08-04 19:23:43 -0400

    fixing styling

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,7 +4,7 @@ function Resume() {
     return (
         <main className="resume-container">
             <section className="container">
-                <img className="has-ratio" width='1344' height='1000' src={process.env.PUBLIC_URL+"/images/AJ Developer resume(portfolio).pdf"} alt="resume"></img>
+                <a className="has-ratio" width='1344' height='1000' href={process.env.PUBLIC_URL+"/images/AJ Developer resume(portfolio).pdf"} alt="resume"></a>
             </section>
         </main>
     )
