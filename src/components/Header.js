536305840508651d29b181755771f193ce629669
Alfredo Figueroa <alfredojfigueroa@gmail.com>
2022-08-04 19:05:43 -0400

    fixing bugs

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ import Home from './home';
 class Header extends Component {
     render() {
         return (
-            <Router>
+            <Router basename='/My-React-Portfolio'>
                 <header>
                     <Navigation />
                 </header>
