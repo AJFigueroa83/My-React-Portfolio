004c1616117ae9a8f1af2f4197bd8da40d3a2b58
Alfredo Figueroa <alfredojfigueroa@gmail.com>
2022-08-04 18:59:34 -0400

    fixing bugs

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ import Home from './home';
 class Header extends Component {
     render() {
         return (
-            <Router basename='/'>
+            <Router>
                 <header>
                     <Navigation />
                 </header>
