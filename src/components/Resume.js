a3425aff4f46d261172744aec06149864723c2a5
Alfredo Figueroa <alfredojfigueroa@gmail.com>
2022-08-06 18:12:12 -0400

    added link to resume and fixed bugs that was not rendering the images.

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,10 +1,12 @@
 import React from "react";
+import logo from '../logo.svg'
+import '../App.css'
 
 function Resume() {
     return (
         <main className="resume-container">
             <section className="container">
-                <a className="has-ratio" width='1344' height='1000' href={process.env.PUBLIC_URL+"/images/AJ Developer resume(portfolio).pdf"} alt="resume"></a>
+                <a className="App-Link" href={process.env.PUBLIC_URL+"/images/AJ Developer resume(portfolio).pdf"} alt="resume"><img src={logo} className='App-logo' alt="logo" />Click logo to view Resume!</a>
             </section>
         </main>
     )
